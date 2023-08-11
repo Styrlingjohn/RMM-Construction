f06323ad2490be27575b5d15ca3280dbce1b44cc
Styrlingjohn <styrling_morris@yahoo.com>
2023-08-11 15:34:58 -0600

    finished aspects of the carousel

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 const ImageSlider = ({ slides }) => {
-    const [currentIndex, setCurrentUser] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
 
     const sliderStyles = {
         height: '100%',
@@ -15,9 +16,43 @@ const ImageSlider = ({ slides }) => {
         backgroundSize: 'cover',
         backgroundImage: `url(${slides[currentIndex].url})`,
     }
+
+    const leftArrowStyles = { 
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(0, -50%)',
+        left: '32px',
+        fontSize: '45px',
+        color: '#fff',
+        zIndex: '1',
+        cursor: 'pointer',
+    };
+    const rightArrowStyles = { 
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(0, -50%)',
+        right: '32px',
+        fontSize: '45px',
+        color: '#fff',
+        zIndex: '1',
+        cursor: 'pointer',
+    };
+    const goToPrevious = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    };
+
+    const goToNext = () => {
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    }
     return (
 
         <div style={sliderStyles}>
+            <div style={leftArrowStyles} onClick={goToPrevious}> ← </div>
+            <div style={rightArrowStyles} onClick={goToNext}> → </div>
             <div style={slideStyles}>
             </div>
         </div>
--- src/components/Pics.js
+++ src/components/Pics.js
@@ -4,23 +4,22 @@ import ImageSlider from './ImageSlider';
 
 const Slider = () => {
   const slides = [
+    { url: 'http://localhost:3000/Image3.jpg', title: 'Image 3' },
     { url: 'http://localhost:3000/Image1.jpg', title: 'Image 1' },
     { url: 'http://localhost:3000/Image2.jpg', title: 'Image 2' },
-    { url: 'http://localhost:3000/Image3.jpg', title: 'Image 3' },
     { url: 'http://localhost:3000/Image4.jpg', title: 'Image 4' },
     { url: 'http://localhost:3000/Image5.jpg', title: 'Image 5' },
 
   ]
 
   const containerStyles = {
-    width: '500px',
-    height: '280px',
-    margin: '0 auto',
+    width: '900px',
+    height: '380px',
+    margin: 'auto',
   };
 
   return (
-    <div>
-      <h1>Hello</h1>
+    <div style={containerStyles}>
       <div style={containerStyles}>
         <ImageSlider slides={slides} />
       </div>
